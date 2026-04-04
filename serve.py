5827b12f4a21c5e4b4e793a14245e1a982392c82
Heather Spacek <heather.spacek@helpwear.ca>
2026-04-03 21:06:20 -0600

    adjusting serve.py

--- serve.py
+++ serve.py
@@ -1,6 +1,6 @@
 from textual_serve.server import Server
 
 server = Server(
-    "pypy3 main.py", host="0.0.0.0", public_url="https://dle.heatherspacek.com"
+    "uv run --python pypy@3.11 main.py", host="0.0.0.0", public_url="https://dle.heatherspacek.com"
 )
 server.serve()
